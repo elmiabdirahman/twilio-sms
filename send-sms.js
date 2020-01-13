f56414e4c4d0de5ed6c19ecf9cc7b45a7b1179aa
Elmi Elmi <elmielmi522@gmail.com>
2020-01-12 21:48:00 -0600

    added .env file and update send-sms server

--- send-sms.js
+++ send-sms.js
@@ -1,11 +1,12 @@
 const accountSid = 'AC2cda3a4ef7609786faf640be2a7dca88';
 const authToken = '9e4104f3a09261e4e76fc7308216d9d0';
+
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
   .create({
-     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-     from: '+15017122661',
-     to: '+15558675310'
+     body: 'hey there',
+     from: +16122301764,
+     to: +16127075119,
    })
   .then(message => console.log(message.sid));
